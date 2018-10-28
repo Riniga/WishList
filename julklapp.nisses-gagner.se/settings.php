5b21c5236c5a258e60575bac8d15f74cd2eac81a
Rickard Nisses-Gagnér <rickard.nisses-gagner@skanska.se>
2018-10-28 16:01:54 +0100

    Remove values in settings :-(

--- julklapp.nisses-gagner.se/settings.php
+++ julklapp.nisses-gagner.se/settings.php
@@ -1,6 +1,6 @@
 <?php
-    $hostname="mydb19.surftown.se";
-    $database="linux_testjulklapp";
-    $username="linux_julklapp";
-    $password="zvSmJKrCYq3m";
+    $hostname="hostname";
+    $database="database";
+    $username="username";
+    $password="password";
 ?>
